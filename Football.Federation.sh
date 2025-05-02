3ce71338e9e69ef9c7c790fa0bf2229d567537f7
Kiarash Sanei <kiarashsanei5@gmail.com>
2025-05-02 22:32:48 +0330

    Modification in bash for running

--- Football.Federation.sh
+++ Football.Federation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+dropdb -U kiarash-sanei -h localhost -p 5432 Football.Federation
+createdb -U kiarash-sanei Football.Federation
 psql -U "kiarash-sanei" -d Football.Federation -f Football.Federation.sql
 psql -U "kiarash-sanei" -d Football.Federation -c "\dt"
--- Student.Association.sh
+++ Student.Association.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+dropdb -U kiarash-sanei -h localhost -p 5432 Student.Association
+createdb -U kiarash-sanei Student.Association
 psql -U "kiarash-sanei" -d Student.Association -f Student.Association.sql
 psql -U "kiarash-sanei" -d Student.Association -c "\dt"
