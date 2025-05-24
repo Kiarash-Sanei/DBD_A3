53b9decbd0252b0cbec281cfcc01baabb974b69e
Kiarash Sanei <kiarashsanei5@gmail.com>
2025-05-24 19:49:03 +0330

    Little changes in ON DELETE

--- Football.Federation.sql
+++ Football.Federation.sql
@@ -20,7 +20,7 @@ CREATE TABLE FootBallClub (
     City TEXT NOT NULL,
     FCName TEXT NOT NULL,
     DateOfEstablishment DATE NOT NULL,
-    FOREIGN KEY (FederationID) REFERENCES Federation(FID)
+    FOREIGN KEY (FederationID) REFERENCES Federation(FID) ON DELETE CASCADE
 );
 
 CREATE TABLE League (
@@ -33,7 +33,7 @@ CREATE TABLE Season (
     SeasonID SERIAL UNIQUE,
     DateOfStart DATE,
     DateOFEnd DATE,
-    FOREIGN KEY (LeagueID) REFERENCES League(LeagueID),
+    FOREIGN KEY (LeagueID) REFERENCES League(LeagueID) ON DELETE CASCADE,
     PRIMARY KEY (LeagueID, SeasonID)
 );
 
@@ -43,8 +43,8 @@ CREATE TABLE Arrangement (
     SeasonID INT NOT NULL,
     Score INT NOT NULL DEFAULT 0 CHECK (Score >= 0),
     Rank INT NOT NULL DEFAULT 1 CHECK (Rank >= 1),
-    FOREIGN KEY (FCID) REFERENCES FootBallClub(FCID),
-    FOREIGN KEY (SeasonID) REFERENCES Season(SeasonID),
+    FOREIGN KEY (FCID) REFERENCES FootBallClub(FCID) ON DELETE CASCADE,
+    FOREIGN KEY (SeasonID) REFERENCES Season(SeasonID) ON DELETE SET NULL,
     PRIMARY KEY (FCID, ID)
 );
 
@@ -53,9 +53,9 @@ CREATE TABLE Player (
     PlayerID SERIAL PRIMARY KEY,
     FCID INT,
     ID INT,
-    FOREIGN KEY (PersonID) REFERENCES Person(PersonID),
-    FOREIGN KEY (FCID) REFERENCES FootBallClub(FCID),
-    FOREIGN KEY (ID) REFERENCES Arrangement(ID)
+    FOREIGN KEY (PersonID) REFERENCES Person(PersonID) ON DELETE CASCADE,
+    FOREIGN KEY (FCID) REFERENCES FootBallClub(FCID) ON DELETE SET NULL,
+    FOREIGN KEY (ID) REFERENCES Arrangement(ID) ON DELETE SET NULL
 );
 
 CREATE TYPE Posts AS ENUM ('Head Coach', 'Coach', 'Doctor', 'Manager');
@@ -64,20 +64,20 @@ CREATE TABLE Staff (
     PersonID INT NOT NULL UNIQUE,
     StaffID SERIAL PRIMARY KEY,
     Post Posts NOT NULL,
-    FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
+    FOREIGN KEY (PersonID) REFERENCES Person(PersonID) ON DELETE CASCADE
 );
 
 CREATE TABLE Referee (
     PersonID INT NOT NULL UNIQUE,
     RefereeID SERIAL PRIMARY KEY,
-    FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
+    FOREIGN KEY (PersonID) REFERENCES Person(PersonID) ON DELETE CASCADE
 );
 
 CREATE TABLE FederationLeagues (
     FID INT,
     LeagueID INT,
-    FOREIGN KEY (FID) REFERENCES Federation(FID),
-    FOREIGN KEY (LeagueID) REFERENCES League(LeagueID),
+    FOREIGN KEY (FID) REFERENCES Federation(FID) ON DELETE CASCADE,
+    FOREIGN KEY (LeagueID) REFERENCES League(LeagueID) ON DELETE CASCADE,
     PRIMARY KEY (FID, LeagueID)
 );
 
@@ -86,7 +86,7 @@ CREATE TABLE WeekTime (
     DateOfStart DATE,
     DateOFEnd DATE,
     WeekID SERIAL UNIQUE,
-    FOREIGN KEY (SeasonID) REFERENCES Season(SeasonID),
+    FOREIGN KEY (SeasonID) REFERENCES Season(SeasonID) ON DELETE CASCADE,
     PRIMARY KEY (SeasonID, WeekID)
 );
 
@@ -94,7 +94,7 @@ CREATE TABLE Game (
     WeekID INT,
     TimeOfGame TIMESTAMP,
     GameID SERIAL UNIQUE,
-    FOREIGN KEY (WeekID) REFERENCES WeekTime(WeekID),
+    FOREIGN KEY (WeekID) REFERENCES WeekTime(WeekID) ON DELETE SET NULL,
     PRIMARY KEY (WeekID, GameID)
 );
 
@@ -102,7 +102,7 @@ CREATE TABLE EventInGame (
     GameID INT,
     EventID SERIAL UNIQUE, 
     TimeOfEvent TIME,
-    FOREIGN KEY (GameID) REFERENCES Game(GameID),
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE,
     PRIMARY KEY (GameID, EventID)
 );
 
@@ -110,8 +110,8 @@ CREATE TABLE Goal (
     EventID INT PRIMARY KEY,
     Kicker INT NOT NULL,
     GoalID SERIAL UNIQUE, 
-    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID),
-    FOREIGN KEY (Kicker) REFERENCES Player(PlayerID)
+    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID) ON DELETE CASCADE,
+    FOREIGN KEY (Kicker) REFERENCES Player(PlayerID) ON DELETE SET NULL
 );
 
 CREATE TABLE Foul (
@@ -119,9 +119,9 @@ CREATE TABLE Foul (
     Doer INT NOT NULL,
     Doee INT NOT NULL,
     FoulID SERIAL UNIQUE, 
-    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID),
-    FOREIGN KEY (Doer) REFERENCES Player(PlayerID),
-    FOREIGN KEY (Doee) REFERENCES Player(PlayerID),
+    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID) ON DELETE CASCADE,
+    FOREIGN KEY (Doer) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
+    FOREIGN KEY (Doee) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
     CONSTRAINT DifferentPlayers CHECK (Doer != Doee)
 );
 
@@ -130,9 +130,9 @@ CREATE TABLE Substitution (
     Comming INT NOT NULL,
     Going INT NOT NULL,
     SubstitutionID SERIAL UNIQUE, 
-    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID),
-    FOREIGN KEY (Comming) REFERENCES Player(PlayerID),
-    FOREIGN KEY (Going) REFERENCES Player(PlayerID),
+    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID) ON DELETE CASCADE,
+    FOREIGN KEY (Comming) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
+    FOREIGN KEY (Going) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
     CONSTRAINT DifferentPlayers CHECK (Comming != Going)
 );
 
@@ -141,9 +141,9 @@ CREATE TABLE YellowCard (
     StaffID INT,
     PlayerID INT,
     YellowCardID SERIAL UNIQUE,
-    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID),
-    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID),
-    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID),
+    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID) ON DELETE CASCADE,
+    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID) ON DELETE SET RESTRICT,
+    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
     CONSTRAINT OneGetter CHECK ((StaffID IS NULL AND PlayerID IS NOT NULL) OR (PlayerID IS NULL AND StaffID IS NOT NULL))
 );
 
@@ -155,9 +155,9 @@ CREATE TABLE RedCard (
     StaffID INT,
     PlayerID INT,
     RedCardID SERIAL UNIQUE, 
-    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID),
-    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID),
-    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID),
+    FOREIGN KEY (EventID) REFERENCES EventInGame(EventID) ON DELETE CASCADE,
+    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID) ON DELETE RESTRICT,
+    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
     CONSTRAINT OneGetter CHECK ((StaffID IS NULL AND PlayerID IS NOT NULL) OR (PlayerID IS NULL AND StaffID IS NOT NULL))
 );
 
@@ -174,23 +174,23 @@ CREATE TABLE Stadium (
 CREATE TABLE Committee (
     SeasonID INT,
     CommitteeID SERIAL UNIQUE,
-    FOREIGN KEY (SeasonID) REFERENCES Season(SeasonID),
+    FOREIGN KEY (SeasonID) REFERENCES Season(SeasonID) ON DELETE CASCADE,
     PRIMARY KEY (SeasonID, CommitteeID)
 );
 
 CREATE TABLE RefereeCanSupervise (
     RefereeID INT,
     LeagueID INT,
-    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID),
-    FOREIGN KEY (LeagueID) REFERENCES League(LeagueID),
+    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID) ON DELETE CASCADE,
+    FOREIGN KEY (LeagueID) REFERENCES League(LeagueID) ON DELETE CASCADE,
     PRIMARY KEY (RefereeID, LeagueID)
 );
 
 CREATE TABLE RefereeIsInCommittee (
     RefereeID INT,
     CommitteeID INT,
-    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID),
-    FOREIGN KEY (CommitteeID) REFERENCES Committee(CommitteeID),
+    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID) ON DELETE CASCADE,
+    FOREIGN KEY (CommitteeID) REFERENCES Committee(CommitteeID) ON DELETE CASCADE,
     PRIMARY KEY (RefereeID, CommitteeID)
 );
 
@@ -201,8 +201,8 @@ CREATE TABLE RefereeIsInGame (
     GameID INT,
     TypeOfReferee RefereeType,
     Score INT CHECK (Score >= 1 AND Score <= 10),
-    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID),
-    FOREIGN KEY (GameID) REFERENCES Game(GameID),
+    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID) ON DELETE SET NULL,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE,
     PRIMARY KEY (RefereeID, GameID)
 );
 
@@ -210,15 +210,15 @@ CREATE TABLE RefereeSupervises (
     RefereeID INT,
     GameID INT PRIMARY KEY,
     DescriptionOfGame TEXT,
-    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID),
-    FOREIGN KEY (GameID) REFERENCES Game(GameID)
+    FOREIGN KEY (RefereeID) REFERENCES Referee(RefereeID) ON DELETE SET NULL,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE
 );
 
 CREATE TABLE PlaceOfGame (
     GameID INT PRIMARY KEY,
     StadiumID INT,
-    FOREIGN KEY (GameID) REFERENCES Game(GameID),
-    FOREIGN KEY (StadiumID) REFERENCES Stadium(StadiumID)
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE,
+    FOREIGN KEY (StadiumID) REFERENCES Stadium(StadiumID) ON DELETE RESTRICT
 );
 
 CREATE TYPE PlayerType AS ENUM ('Main', 'Substitution');
@@ -227,16 +227,16 @@ CREATE TABLE Plays(
     PlayerID INT NOT NULL,
     GameID INT NOT NULL,
     TypeOfPlayer PlayerType,
-    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID),
-    FOREIGN KEY (GameID) REFERENCES Game(GameID),
+    FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID) ON DELETE RESTRICT,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE,
     PRIMARY KEY (PlayerID, GameID)
 );
 
 CREATE TABLE Participate (
     StaffID INT NOT NULL,
     GameID INT NOT NULL,
-    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID),
-    FOREIGN KEY (GameID) REFERENCES Game(GameID),
+    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID) ON DELETE RESTRICT,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE,
     PRIMARY KEY (StaffID, GameID)
 );
 
@@ -261,7 +261,7 @@ CREATE TABLE GamesDone (
     Offside INT NOT NULL DEFAULT 0 CHECK (Offside >= 0),
     Corner INT NOT NULL DEFAULT 0 CHECK (Corner >= 0),
     Dangerous INT NOT NULL DEFAULT 0 CHECK (Dangerous >= 0),
-    FOREIGN KEY (ID) REFERENCES Arrangement(ID),
-    FOREIGN KEY (GameID) REFERENCES Game(GameID),
+    FOREIGN KEY (ID) REFERENCES Arrangement(ID) ON DELETE RESTRICT,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID) ON DELETE CASCADE,
     PRIMARY KEY (ID, GameID)
-)
+);
